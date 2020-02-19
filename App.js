c8e0c6b93155fae41474908e82b0dd834e6e171d
Alfredo-Rojas <alfredot84@gmail.com>
2020-02-19 15:29:12 +0800

    Chart styling adding

--- App.js
+++ App.js
@@ -1,49 +1,63 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {ScrollView, StyleSheet, View, Text} from 'react-native';
 import EChartComponent from './src/components/echarts/EChartComponent';
 // import {ECharts} from 'native-echarts';
 
 // import Chart from './src/screens/NativeChartKit';
 
-const option = {
-  title: {
-    text: 'Chart Title',
+const seriesData = [
+  {
+    legendData: ['1 - Series1', '1 - Series2'],
+    colors: ['#ff0000', '#fff'],
+    data: [
+      [820, 932, 901, 934, 1290, 1330, 1320, 789, 998],
+      [232, 423, 533, 633, 123, 754, 1432, 345, 657],
+    ],
   },
-  legend: {
-    icon: 'circle',
-    data: ['legend1'],
+  {
+    lowerYLimit: 300,
+    upperYLimit: 700,
+    legendData: ['2 - Series1'],
+    colors: ['#ff0000'],
+    data: [[232, 423, 533, 633, 123, 754, 1432, 345, 657]],
   },
-  xAxis: {
-    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  {
+    legendData: ['3 - Series1', '3 - Series2'],
+    colors: ['#ff0000', '#fff'],
+    data: [
+      [820, 932, 901, 256, 234, 333, 234, 633, 998],
+      [232, 423, 533, 863, 123, 754, 1432, 345, 657],
+    ],
   },
-  yAxis: {
-    show: false,
-    splitLine: {
-      show: false,
-    },
+  {
+    legendData: ['4 - Series1', '4 - Series2'],
+    colors: ['#ff0000', '#fff'],
+    data: [
+      [820, 932, 901, 934, 111, 444, 666, 789, 998],
+      [232, 423, 533, 633, 123, 754, 789, 345, 657],
+    ],
   },
-  series: [
-    {
-      name: 'legend1',
-      data: [820, 932, 901, 934, 1290, 1330, 1320],
-      type: 'bar',
-    },
-  ],
-};
+];
 
 const App = () => {
   return (
-    <View style={styles.appView}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Custom Chart Wrapper</Text>
+    <ScrollView>
+      <View style={styles.appView}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Custom Chart Wrapper</Text>
+        </View>
+        <View style={styles.dateController}>
+          <Text>Date Control</Text>
+        </View>
+        <View style={styles.chartContainer}>
+          {seriesData.map((serieData, index) => (
+            <View key={`serie-${index}`}>
+              <EChartComponent {...serieData} />
+            </View>
+          ))}
+        </View>
       </View>
-      <View style={styles.dateController}>
-        <Text>Date Control</Text>
-      </View>
-      <View style={styles.chartContainer}>
-        <EChartComponent option={option} />
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 
--- src/components/echarts/EChartComponent.js
+++ src/components/echarts/EChartComponent.js
@@ -1,20 +1,96 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {StyleSheet, SafeAreaView} from 'react-native';
 import {ECharts} from 'react-native-echarts-wrapper';
 
-function EChartComponent({option}) {
+function getDefaultOption() {
+  return {
+    title: {
+      text: 'Chart Title',
+      textStyle: {
+        color: '#fff',
+      },
+      left: 10,
+    },
+    grid: {
+      left: 0,
+      right: 0,
+      bottom: 60,
+      top: 70,
+    },
+    legend: {
+      left: 10,
+      top: 30,
+      icon: 'circle',
+      // data: ['legend1', 'legend2'],
+      textStyle: {
+        color: '#fff',
+        fontSize: 16,
+      },
+    },
+    xAxis: {
+      data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+      axisTick: {
+        show: false,
+      },
+      axisLine: {
+        show: false,
+      },
+    },
+    yAxis: {
+      show: false,
+      splitLine: {
+        show: false,
+      },
+    },
+    series: [],
+    backgroundColor: 'rgba(57,57,57,0.9)',
+    textStyle: {
+      color: '#fff',
+    },
+  };
+}
+const serieOption = {
+  // name: 'legend2',
+  // data: [232, 423, 533, 633, 123, 754, 1432, 345, 657],
+  type: 'bar',
+  barMaxWidth: '20%',
+};
+// data: [[232, 423, 533, 633, 123, 754, 1432, 345, 657]],
+const EChartComponent = ({
+  lowerYLimit = 0,
+  upperYLimit = 0,
+  legendData = [],
+  data = [],
+  colors = [],
+}) => {
+  const [chartOption, setChartOption] = useState(getDefaultOption());
+  useEffect(() => {
+    const defaultOption = Object.assign({}, getDefaultOption());
+    defaultOption.legend.data = legendData;
+    defaultOption.series = data.map((item, index) => ({
+      ...serieOption,
+      data: item,
+      name: legendData[index],
+      itemStyle: {
+        color: colors[index] || '#fff',
+      },
+    }));
+    setChartOption(defaultOption);
+  }, [legendData, data, colors]);
+
   return (
     <SafeAreaView style={styles.chartContainer}>
-      <ECharts option={option} width="100%" />
+      <ECharts option={chartOption} />
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   chartContainer: {
     backgroundColor: '#ff00ff',
-    height: 300,
+    height: 240,
     margin: 10,
+    borderRadius: 4,
   },
 });
 
